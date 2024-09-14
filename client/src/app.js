6c889648b0539cc1d13974c10a103969e7c04ac5
leorasaharia <saharialeora2406@gmail.com>
2024-09-14 14:39:54 +0530

    signin,signup page reinitialized and page.js was rendered

--- client/src/app.js
+++ client/src/app.js
@@ -1,1 +1,16 @@
-//main
+
+import Link from 'next/link';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <Link href="/signin">
+        <a>Sign In</a>
+      </Link>
+      <Link href="/signup">
+        <a>Sign Up</a>
+      </Link>
+    </div>
+  );
+}
