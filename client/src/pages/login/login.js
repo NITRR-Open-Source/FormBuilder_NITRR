7504a1b8c422d49f2fefc37b47135b55c9e63a0c
Itsraj786@iul <ransari078.btech2023@mme.nitrr.ac.in>
2024-09-24 15:59:21 +0530

    seamless navigation between the login and signup page

--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -1,29 +1,31 @@
-import "../../app/globals.css"
+import Link from 'next/link';
+import "../../app/globals.css";
 
 export default function Login() {
     return (
-      <div className="flex h-screen">
-        <div className="flex-1 bg-gradient-to-r from-blue-800 via-blue-600 to-blue-400 text-white flex flex-col justify-center items-center text-center rounded-r-[50px] p-12">
-          <div className="mb-6">
-            <h1 className="text-3xl font-bold">Welcome to</h1>
-          </div>
-          <p>
-            ICell Form Builder Web is your go-to solution for creating, managing, and deploying professional forms easily. Whether you're building a simple contact form or a complex survey, we've got the tools to help you succeed.
-          </p>
-  
-  
-          <div className='flex'>
-            <button type="button" className="bg-white text-blue-800 border border-blue-800 px-4 py-2 rounded hover:bg-green-300 m-[15px]">Login</button>
-            <button type="button" className="bg-white text-blue-800 border border-blue-800 px-4 py-2 rounded hover:bg-blue-100 m-[15px]">Sign up</button>
-          </div>
-        </div>
-  
-  
-  
-        <div className="flex-1 bg-white p-12 overflow-y-auto rounded-l-[50px] ml-[10px]">
-          <h2 className="text-2xl font-bold mb-5 text-black">Login in to your account</h2>
-          <form>
-          <div className="mb-4">
+        <div className="flex h-screen">
+            <div className="flex-1 bg-gradient-to-r from-blue-800 via-blue-600 to-blue-400 text-white flex flex-col justify-center items-center text-center rounded-r-[50px] p-12">
+                <div className="mb-6">
+                    <h1 className="text-3xl font-bold">Welcome to</h1>
+                </div>
+                <p>
+                    ICell Form Builder Web is your go-to solution for creating, managing, and deploying professional forms easily. Whether you're building a simple contact form or a complex survey, we've got the tools to help you succeed.
+                </p>
+                
+                <div className='flex'>
+                    <Link href="/login/login" className="bg-white text-blue-800 border border-blue-800 px-4 py-2 rounded hover:bg-green-300 m-[15px]">
+                        Login
+                    </Link>
+                    <Link href="/signup/signup" className="bg-white text-blue-800 border border-blue-800 px-4 py-2 rounded hover:bg-blue-100 m-[15px]">
+                        Sign up
+                    </Link>
+                </div>
+            </div>
+
+            <div className="flex-1 bg-white p-12 overflow-y-auto rounded-l-[50px] ml-[10px]">
+                <h2 className="text-2xl font-bold mb-5 text-blue-600">Login to your account</h2>
+                <form>
+                    <div className="mb-4">
                         <label htmlFor="email" className="block font-bold mb-2 text-black">E-mail Address</label>
                         <input
                             type="email"
@@ -32,7 +34,7 @@ export default function Login() {
                             className="w-full p-3 border border-blue-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black text-lg"
                         />
                     </div>
-  
+
                     <div className="mb-4">
                         <label htmlFor="password" className="block font-bold mb-2 text-black">Password</label>
                         <input
@@ -42,28 +44,25 @@ export default function Login() {
                             className="w-full p-3 border border-blue-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black text-lg"
                         />
                     </div>
-  
-            <div className="flex justify-between mt-6 text-black" >
-              <button type="button" className="bg-blue-800 text-white px-4 py-2 rounded hover:bg-blue-900">Login</button>
+
+                    <div className="flex justify-between mt-6 text-black">
+                        <button type="submit" className="bg-blue-800 text-white px-4 py-2 rounded hover:bg-blue-900">
+                            Login
+                        </button>
+                    </div>
+                </form>
+
+                <div className="text-center mt-5 text-black">
+                    <p>
+                        Don't have an account?{' '}
+                        <Link href="/signup/signup" className="border text-blue-800 rounded hover:bg-green-800 hover:text-white">
+                            Sign Up
+                        </Link>
+                    </p>
+                    <p>OR</p>
+                    <p>If you have any problems, contact the technical team.</p>
+                </div>
             </div>
-          </form>
-  
-          <div className="text-center mt-5 text-black">
-            <p>
-              Don't have an account?{' '}
-              <button
-                type="button"
-                className=" border text-blue-800  rounded hover:bg-green-800 hover:text-white">
-                Sign Up
-              </button>
-            </p>
-            <p>OR</p>
-            <p>If you have any problems, contact the technical team.</p>
-  
-          </div>
-  
-  
         </div>
-      </div >
     );
-  }
+}
--- client/src/pages/signup/signup.js
+++ client/src/pages/signup/signup.js
@@ -1,28 +1,31 @@
+import Link from 'next/link';
 import "../../app/globals.css";
 
 export default function SignUp() {
     return (
         <div className="flex h-screen">
-            <div className="flex-1 bg-blue-600 text-white p-12 rounded-3xl m-2 flex flex-col justify-center items-center text-center">
-                <div className="mb-8">
-                    <h1 className="text-5xl font-bold">Welcome to</h1>
-                    <h2 className="text-4xl font-semibold mt-3">ICell Form Builder Web</h2>
+            <div className="flex-1 bg-gradient-to-r from-blue-800 via-blue-600 to-blue-400 text-white flex flex-col justify-center items-center text-center rounded-r-[50px] p-12">
+                <div className="mb-6">
+                    <h1 className="text-3xl font-bold">Welcome to</h1>
                 </div>
-             
-                <p className="max-w-xl text-lg mb-10">
-                    Your go-to solution for creating, managing, and deploying professional forms easily. Whether you're building a simple contact form or a complex survey, we've got the tools to help you succeed.
+                <p>
+                    ICell Form Builder Web is your go-to solution for creating, managing, and deploying professional forms easily. Whether you're building a simple contact form or a complex survey, we've got the tools to help you succeed.
                 </p>
-                <div className="flex">
-                  
-                    <button type="button" className="bg-white text-blue-600 px-8 py-3 rounded-full text-lg border-2 border-blue-600 hover:border-blue-800 hover:bg-blue-600 hover:text-white transition duration-300 m-[15px]">Login</button>
-                    <button type="button" className="bg-blue-500 text-white px-8 py-3 rounded-full text-lg border-2 border-blue-700 hover:border-white transition duration-300 m-[15px]">Sign Up</button>
+                
+                <div className='flex'>
+                    <Link href="/login/login" className="bg-white text-blue-800 border border-blue-800 px-4 py-2 rounded hover:bg-green-300 m-[15px]">
+                        Login
+                    </Link>
+                    <Link href="/signup/signup" className="bg-white text-blue-800 border border-blue-800 px-4 py-2 rounded hover:bg-blue-100 m-[15px]">
+                        Sign up
+                    </Link>
                 </div>
             </div>
 
             <div className="flex-1 bg-white p-12 rounded-3xl m-2 overflow-y-auto">
                 <h2 className="text-2xl font-bold mb-5 text-blue-600">Create your account</h2>
                 <form>
-                    {/* Input Group - First Name */}
+                    {/* Input Fields */}
                     <div className="mb-4">
                         <label htmlFor="first-name" className="block font-bold mb-2 text-black">First Name</label>
                         <input
@@ -33,7 +36,6 @@ export default function SignUp() {
                         />
                     </div>
 
-                    {/* Input Group - Last Name */}
                     <div className="mb-4">
                         <label htmlFor="last-name" className="block font-bold mb-2 text-black">Last Name</label>
                         <input
@@ -44,7 +46,6 @@ export default function SignUp() {
                         />
                     </div>
 
-                    {/* Input Group - E-mail */}
                     <div className="mb-4">
                         <label htmlFor="email" className="block font-bold mb-2 text-black">E-mail Address</label>
                         <input
@@ -55,7 +56,6 @@ export default function SignUp() {
                         />
                     </div>
 
-                    {/* Input Group - Password */}
                     <div className="mb-4">
                         <label htmlFor="password" className="block font-bold mb-2 text-black">Password</label>
                         <input
@@ -66,7 +66,6 @@ export default function SignUp() {
                         />
                     </div>
 
-                    {/* Input Group - Contact Number */}
                     <div className="mb-4">
                         <label htmlFor="contact" className="block font-bold mb-2 text-black">Contact Number</label>
                         <input
@@ -77,7 +76,6 @@ export default function SignUp() {
                         />
                     </div>
 
-                    {/* Input Group - Club / Committee Name */}
                     <div className="mb-4">
                         <label htmlFor="club-name" className="block font-bold mb-2 text-black">Club / Committee Name</label>
                         <input
@@ -88,7 +86,6 @@ export default function SignUp() {
                         />
                     </div>
 
-                    {/* Input Group - Your Position */}
                     <div className="mb-4">
                         <label htmlFor="position" className="block font-bold mb-2 text-black">Your Position</label>
                         <input
@@ -99,7 +96,6 @@ export default function SignUp() {
                         />
                     </div>
 
-                    {/* Input Group - Security Key */}
                     <div className="mb-4">
                         <label htmlFor="security-key" className="block font-bold mb-2 text-black">Security Key</label>
                         <input
@@ -109,32 +105,22 @@ export default function SignUp() {
                             className="w-full p-3 border border-blue-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black text-lg"
                         />
                     </div>
-                    
 
-                    <div className="flex justify-center mt-6 text-black">
-                        <button
-                            type="submit"
-                            className="w-[48%] bg-blue-600 text-white p-3 rounded-full border-2 border-blue-600 hover:border-white active:bg-blue-700 active:scale-95 transition-all duration-300">
+                    <div className="flex justify-between mt-6 text-black">
+                        <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-800">
                             Sign Up
                         </button>
                     </div>
-               
                 </form>
-                
 
-                <div className="text-center mt-8 text-blue-600">
+                <div className="text-center mt-5 text-black">
                     <p>
                         Already have an account?{' '}
-                        <button
-                            type="button"
-                            className="font-semibold underline hover:text-blue-800">
+                        <Link href="/login/login" className="border text-blue-600 rounded hover:bg-green-800 hover:text-white">
                             Login
-                        </button>
+                        </Link>
                     </p>
-                    <p className="mt-2">OR</p>
-                    <p>If you have any problems, contact the technical team.</p>
                 </div>
-                
             </div>
         </div>
     );
